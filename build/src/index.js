d3fddd60f636de192cd6d43980dc21cf547058f0
max4779 <126331668+max4779@users.noreply.github.com>
2024-11-11 21:34:58 +0900

    Update index.js

--- build/src/index.js
+++ build/src/index.js
@@ -1,17 +1,17 @@
-// import * as LoaderUtils from './loaders/Utils.js';
-// import { SplatBuffer } from './loaders/SplatBuffer.js';
-// import { SplatParser } from './loaders/splat/SplatParser.js';
-// import { SplatPartitioner } from './loaders/SplatPartitioner.js';
-// import { SplatBufferGenerator } from './loaders/SplatBufferGenerator.js';
+import * as LoaderUtils from './loaders/Utils.js';
+import { SplatBuffer } from './loaders/SplatBuffer.js';
+import { SplatParser } from './loaders/splat/SplatParser.js';
+import { SplatPartitioner } from './loaders/SplatPartitioner.js';
+import { SplatBufferGenerator } from './loaders/SplatBufferGenerator.js';
 import { Viewer } from './Viewer.js';
-// import { DropInViewer } from './DropInViewer.js';
-// import { OrbitControls } from './OrbitControls.js';
-// import { AbortablePromise } from './AbortablePromise.js';
-// import { SceneFormat } from './loaders/SceneFormat.js';
-// import { RenderMode } from './RenderMode.js';
-// import { LogLevel } from './LogLevel.js';
-// import { SceneRevealMode } from './SceneRevealMode.js';
-// import { SplatRenderMode } from './SplatRenderMode.js';
+import { DropInViewer } from './DropInViewer.js';
+import { OrbitControls } from './OrbitControls.js';
+import { AbortablePromise } from './AbortablePromise.js';
+import { SceneFormat } from './loaders/SceneFormat.js';
+import { RenderMode } from './RenderMode.js';
+import { LogLevel } from './LogLevel.js';
+import { SceneRevealMode } from './SceneRevealMode.js';
+import { SplatRenderMode } from './SplatRenderMode.js';
 
 export {
     PlyParser,
